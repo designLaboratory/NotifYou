a4f8280ac2ed1a14b6eecb22bb376184cf29fded
Damian Mucha <mdamian@student.agh.edu.pl>
2017-01-05 22:38:51 +0100

    Added startup of controlled device

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'light_my_fire' 
+ * Target:  'J-LINK DEVICE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "MKL46Z4.h"
+
+
+#endif /* RTE_COMPONENTS_H */
